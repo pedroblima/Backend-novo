a094ca71dee221e379cb76a79c8d315ee8bf7c25
pedroblima <ppedrohenrique1@gmail.com>
2024-03-13 20:07:49 -0300

    Lista 2 e 3

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.use(express.urlencoded({extended: false}))
 const introducao = require('./routes/introducao')
 const N1 = require('./routes/N1')
 const N2 = require('./routes/N2')
+const N3 = require('./routes/N3')
 
 app.use('/', introducao)
 app.use('/N1', N1)
 app.use('/N2', N2)
+app.use('/N3', N3)
 
 app.listen(3000, function(){
     console.log('Server UP port 3000')
