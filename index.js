21bc3e2558335178fae79777cce5cc9a4f5ff813
pedroblima <pedrob@iesb.edu.br>
2024-03-13 16:08:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+const introducao = require('./routes/introducao')
+const lista1 = require('./routes/lista1')
+const lista2 = require('./routes/lista2')
+
+app.use('/', introducao)
+app.use('/lista1', lista1)
+app.use('/lista2', lista2)
+
+app.listen(3000, function(){
+    console.log('Server UP port 3000')
+})
