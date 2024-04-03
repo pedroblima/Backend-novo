1c010c4f713858de6c3c1ea8cd2288d7de258c45
pedroblima <pedrob@iesb.edu.br>
2024-04-03 07:51:42 -0300

    atividade 5

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const N1 = require('./routes/N1')
 const N2 = require('./routes/N2')
 const N3 = require('./routes/N3')
 const N4 = require('./routes/N4')
+const lista5 = require('./routes/lista5')
 
 app.use('/', introducao)
 app.use('/N1', N1)
 app.use('/N2', N2)
 app.use('/N3', N3)
 app.use('/N4', N4)
+app.use('/lista5', lista5)
 
 app.listen(3000, function(){
     console.log('Server UP port 3000')
