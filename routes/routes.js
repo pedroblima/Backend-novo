a94b83991c46a47f05cbc92c30f74df1859b54b8
pedroblima <ppedrohenrique1@gmail.com>
2024-04-17 21:58:12 -0300

    revisao

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 
-const AlunoController = require('../controller/AlunosController')
+const CargoController = require('../controller/CargoController')
 
-router.get('/', function(req, res){
+router.get('/', function(req,res){
     res.json({})
 })
 
@@ -14,10 +14,5 @@ router.put('/alunos/:id', (req, res) => AlunoController.update(req,res))
 router.get('/alunos/:id', (req, res) => AlunoController.get(req, res))
 router.delete('/alunos/:id', (req, res) => AlunoController.delete(req,res))
 
-router.put('/migrar', (req, res) => AlunoController.migrar(req, res));
-router.get('/aprovados', (req, res) => AlunoController.Aprovados(req, res));
-router.get('/reprovados', (req, res) => AlunoController.Reprovados(req, res));
-router.get('/recuperacao', (req, res) => AlunoController.Recuperacao(req, res));
-router.delete('/excluirAlunos', (req, res) => AlunoController.excluirAlunos(req, res));
 
 module.exports = router
